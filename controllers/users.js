9c9f7c52950c838d032c52f7f0353bb0ec1a5c53
Andreas St <andreas_st@outlook.com>
2022-07-05 23:41:35 +0300

    Second Commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-
 const User = require("../models/user");
 
 module.exports.renderRegister = (req, res) => {
@@ -7,19 +6,19 @@ module.exports.renderRegister = (req, res) => {
 
 module.exports.register = async (req, res) => {
   try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username, password })
-      const registeredUser = await User.register(user, password)
-      req.login(registeredUser, err => {
-          if (err) return next(err);
-          req.flash('success', 'Welcome to covid detection app!')
-          res.redirect('/pois')
-      })
+    const { email, username, password } = req.body;
+    const user = new User({ email, username, password });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to covid detection app!");
+      res.redirect("/pois");
+    });
   } catch (e) {
-      req.flash('error', e.message)
-      res.redirect('register')
+    req.flash("error", e.message);
+    res.redirect("register");
   }
-}
+};
 
 module.exports.renderLogin = (req, res) => {
   res.render("users/login");
@@ -32,8 +31,12 @@ module.exports.login = (req, res) => {
   res.redirect(redirectUrl);
 };
 
-module.exports.logout = (req, res) => {
-  req.logout();
-  req.flash("success", `Goodbye  ${res.locals.currentUser.username}`);
-  res.redirect("/pois");
+module.exports.logout = (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", `Goodbye  ${res.locals.currentUser.username}`);
+    res.redirect("/pois");
+  });
 };
